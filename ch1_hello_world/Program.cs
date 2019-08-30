c17e6f21f75d1add040576ff8e85bb2a6c681c85
Blaise Reints <blaise@remyhealth.com>
2019-08-30 09:27:44 -0500

    initial commit

---
+++ ch1_hello_world/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ch1_hello_world
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string hello = "hello world";
+            DateTime now = DateTime.Now;
+            Console.Write(hello);
+            Console.WriteLine("the date is " + now.ToLongDateString());
+        }
+    }
+}
